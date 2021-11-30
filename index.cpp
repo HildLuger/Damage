61b59caf8bb2212fb746871a7b5711882ef7fbcf
HildLuger <88519194+HildLuger@users.noreply.github.com>
2021-11-30 13:16:45 -0300

    Add files via upload

--- index.cpp
+++ index.cpp
@@ -5,10 +5,13 @@ int main()
 {
     int strength;
     int weapon;
+    int yes;
     printf("Imput your character's strength: ");
     scanf("%d", &strength);
     printf("Imput your weapon damage:");
     scanf("%d", &weapon);
     printf("Your total damage is: %d", strength + weapon);
+    printf("\n\nType \"yes\" to exit the program:");
+    scanf("%d", &yes);
     return 0;
 }
